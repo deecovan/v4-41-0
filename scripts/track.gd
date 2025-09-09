5219b30fc5d84d46ee71c444bb0dcec491b075e4
Dee <dee.covan@gmail.com>
2025-09-10 02:07:23 +0300

    Why its movin?

--- scripts/track.gd
+++ scripts/track.gd
@@ -3,10 +3,14 @@ extends Node2D
 var path: Path2D
 var pathFollow: PathFollow2D
 var line: Line2D
+@export var trackDetails: int = 100
+@export var wallsPackedScene: PackedScene
+var loadedWalls
 
 func _ready() -> void:
 	path = $Path2D
 	pathFollow = $Path2D/PathFollow2D
+	loadedWalls = preload("res://scenes/walls.tscn")
 	drawTrack()
 
 func drawTrack() -> void:
@@ -19,4 +23,19 @@ func drawTrack() -> void:
 		line.add_point(point + path.position)
 	get_parent().add_child.call_deferred(line)
 	## PathFollow processed track
-	
+	var pathPoint: float
+	var pathPointPosition
+	var pathPointRotation
+	for i in trackDetails:
+		pathPoint = float(i)/trackDetails
+		pathFollow.progress_ratio = pathPoint
+		pathPointPosition = pathFollow.position
+		pathPointRotation = pathFollow.rotation
+		printt(i, trackDetails, pathPoint, pathPointPosition, pathPointRotation)
+		## Instantiate Walls at this point
+		var newWalls = loadedWalls.instantiate()
+		newWalls.name = "walls" + str(i)
+		newWalls.position = pathPointPosition
+		newWalls.rotation = pathPointRotation
+		add_child(newWalls)
+		
