d0a42247b00ea9dd5d72b366be2635b2bfc19f86
Dee <dee.covan@gmail.com>
2025-09-14 08:57:50 +0300

    Prepare Add cars from v4-41-racing-ai PlayerA

--- scripts/track.gd
+++ scripts/track.gd
@@ -3,7 +3,7 @@ extends Node2D
 var path: Path2D
 var pathFollow: PathFollow2D
 var line: Line2D
-@export var trackDetails: int = 100
+@export var trackDetails: int = 30
 @export var wallsPackedScene: PackedScene
 var loadedWalls
 
@@ -18,24 +18,26 @@ func drawTrack() -> void:
 	line = Line2D.new()   
 	line.set_antialiased(true)
 	line.default_color = Color(0.5,0.5,1,0.5)  
-	line.width = 10
-	for point in path.curve.get_baked_points():  
-		line.add_point(point + path.position)
-	get_parent().add_child.call_deferred(line)
-	## PathFollow processed track
-	var pathPoint: float
-	var pathPointPosition
-	var pathPointRotation
-	for i in trackDetails:
-		pathPoint = float(i)/trackDetails
-		pathFollow.progress_ratio = pathPoint
-		pathPointPosition = pathFollow.position
-		pathPointRotation = pathFollow.rotation
-		printt(i, trackDetails, pathPoint, pathPointPosition, pathPointRotation)
-		## Instantiate Walls at this point
-		var newWalls = loadedWalls.instantiate()
-		newWalls.name = "walls" + str(i)
-		newWalls.position = pathPointPosition
-		newWalls.rotation = pathPointRotation
-		add_child(newWalls)
+	line.width = 20
+	## Draw walls
+	# Remember Last Point to find rotation vector from it
+	var lastPoint = Vector2.ZERO
+	var lastUnusedPoint = Vector2.ZERO
+	var i = 0
+	for point in path.curve.get_baked_points():
+		var diretionTo = lastUnusedPoint.direction_to(point).angle()
+		var distanceTo = Vector2(lastPoint - point).length()
+		if distanceTo > trackDetails and lastUnusedPoint != Vector2.ZERO:
+			var newWalls = loadedWalls.instantiate()
+			newWalls.name = "walls" + str(i)
+			i += 1
+			newWalls.position = point
+			newWalls.rotation = diretionTo
+			add_child(newWalls)
+			lastPoint = point
+			#printt(lastPoint, point, diretionTo)
+		lastUnusedPoint = point
 		
+		line.add_point(point)
+		
+	get_parent().add_child.call_deferred(line)
